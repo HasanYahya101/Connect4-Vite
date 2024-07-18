642718011a4d576f959d444b985718ee515c082e
Hasan Yahya <hasna786yahya@gmail.com>
2024-07-18 07:16:22 +0500

    chore: Add @radix-ui/react-alert-dialog and @radix-ui/react-slot dependencies, update package.json, and implement apology component for non-desktop users

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import Connect4 from './components/component/playground';
+import Apology from './components/component/apology';
+
+const isDesktop = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    const isMobile = /mobile|android|iphone|ipad|tablet|touch|samsung|fridge/i.test(userAgent);
+    const isSmallScreen = window.innerWidth <= 1024;
+    return !isMobile && !isSmallScreen;
+};
 
 function App() {
-    return (
-        <Connect4 />
-    )
+    if (!isDesktop()) {
+        return <Apology />;
+    } else {
+        return (
+            <Connect4 />
+        )
+    }
 }
 
 export default App
