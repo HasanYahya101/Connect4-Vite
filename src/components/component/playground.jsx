4f72f30fb302702a165d21e86f92ceb11c693440
Hasan Yahya <118683092+HasanYahya101@users.noreply.github.com>
2024-07-18 08:11:55 +0500

    feat: fix message errors

--- src/components/component/playground.jsx
+++ src/components/component/playground.jsx
@@ -110,7 +110,7 @@ const Connect4 = () => {
                         <TooltipContent className='p-6'>
                             <h1 className="text-xl mb-3 font-semibold">Connect 4</h1>
                             <p className="text-sm max-w-md text-muted-foreground"
-                            >Connect 4 is a two-player connection game in which the players first choose a color and then take turns dropping colored discs from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one\'s own discs.
+                            >Connect 4 is a two-player connection game in which the players first choose a color and then take turns dropping colored discs from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs.
                             </p>
                         </TooltipContent>
                     </Tooltip>
